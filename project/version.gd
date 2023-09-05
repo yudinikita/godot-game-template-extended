0da7e30c496548c942bbf535c2a992d1d3843acb
Nikita Yudin <nikitayudin782@gmail.com>
2023-09-05 21:12:48 +0300

    ci: add release-please

---
+++ project/version.gd
@@ -0,0 +1,17 @@
+const NAME := "Godot Game Template Extended"
+
+# x-release-please-start-version
+const VERSION := ""
+# x-release-please-end
+
+# x-release-please-start-major
+const MAJOR := 0
+# x-release-please-end
+
+# x-release-please-start-minor
+const MINOR := 0
+# x-release-please-end
+
+# x-release-please-start-patch
+const PATCH := 0
+# x-release-please-end
