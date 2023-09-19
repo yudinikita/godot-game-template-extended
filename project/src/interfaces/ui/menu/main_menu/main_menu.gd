7628b6e07b12d1decb72aa77263cdb3e59f8d8e6
Nikita Yudin <nikitayudin782@gmail.com>
2023-09-19 19:51:09 +0300

    feat: add main menu

---
+++ project/src/interfaces/ui/menu/main_menu/main_menu.gd
@@ -0,0 +1,32 @@
+extends Control
+
+@onready var video_bg := $Control/VideoStreamPlayer as VideoStreamPlayer
+@onready var play_btn := $MC/MC2/HBC/VBC/VBC2/Play as Button
+@onready var quit_btn := $MC/MC2/HBC/VBC/VBC2/Quit as Button
+@onready var confirm_quit := $ConfirmQuit as Control
+@onready var version := $MC/Version as Label
+@onready var project_version := preload("res://version.gd")
+
+
+func _ready():
+	SceneManager.reset_scene_manager()
+	version.text = ProjectVersion.get_version()
+	play_btn.grab_focus()
+
+
+func _on_video_stream_player_finished():
+	video_bg.play()
+
+
+func _on_quit_button_up():
+	confirm_quit.visible = true
+
+
+func _on_confirm_quit_confirmed():
+	SceneManager.change_scene(
+		"quit", quit_btn.fade_out_options, quit_btn.fade_in_options, quit_btn.general_options
+	)
+
+
+func _on_confirm_quit_canceled():
+	confirm_quit.visible = false
